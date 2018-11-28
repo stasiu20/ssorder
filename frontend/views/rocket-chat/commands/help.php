d1f238b9fc4a01ea1d5cb01657ef6fbfa4f30a25
Marcin Morawski <marcin@morawskim.pl>
2018-11-28 16:59:15 +0100

    Fix literowki

--- frontend/views/rocket-chat/commands/help.php
+++ frontend/views/rocket-chat/commands/help.php
@@ -2,5 +2,5 @@ Dostępne polecenia to:
 * info - zwraca id uzytkownika i informacje o powiazaniu (lub nie) konta z ssorder
 * order - informacja o dzisiejszym zamowieniu lub braku
 * last - informacja o ostatnim zrealizowanym zamowieniu (ale nie w dniu dzisiejszym)
-* history - YYYY-MM-DD (zamowienia z danego dnia) | -5days (zamowienie z przed 5dni)
+* history - YYYY-MM-DD (zamowienia z danego dnia) | -5days (zamowienie sprzed 5dni)
 * reorder NrZamownia [uwagi]
