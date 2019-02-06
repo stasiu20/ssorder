99f17652f7ed10737bd6d929ad51986824646f92
Marcin Morawski <marcin@morawskim.pl>
2019-02-06 17:01:28 +0100

    Set correct db connection params

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=mysql;dbname=ssorder;port=3306',
+            'username' => 'ssorder',
+            'password' => 'ssorderpassword',
             'charset' => 'utf8',
         ],
         'mailer' => [
@@ -14,7 +14,12 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            #'useFileTransport' => true,
         ],
+        'rocketChat' => [
+            'endpoint' => 'http://rocketchat.lvh.me:3000',
+            'token' => 'puhPSTPcA6zfXQtZZ/9dbe4Az3S72e4fFbpTaY3fDMufRszeZaTuQfaESaCp8cSzkk',
+        ]
     ],
+
 ];
