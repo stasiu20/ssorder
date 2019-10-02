2454423b7eb8c668070c269af3656c35eb4cdb2a
Marcin Morawski <marcin@morawskim.pl>
2019-10-02 18:10:13 +0200

    #79 - Obsługa błędów js

---
+++ frontend/webpack/js/errorHandler.js
@@ -0,0 +1,27 @@
+/* global ga */
+import ErrorStackParser from 'error-stack-parser';
+
+window.addEventListener('error', function(e) {
+    e.preventDefault();
+    const { message, filename, lineno, colno, error } = e;
+    let stack = null;
+    if (error && error instanceof Error) {
+        stack = ErrorStackParser.parse(error) || [];
+    }
+    const userAgent = navigator.userAgent;
+    let errorObj = { message, filename, lineno, colno, stack, userAgent };
+    console.error(errorObj);
+    ga('send', 'exception', {
+        exDescription: JSON.stringify(errorObj),
+        exFatal: true,
+    });
+    ga(
+        'send',
+        'event',
+        'window.error',
+        message,
+        JSON.stringify(errorObj),
+        undefined,
+        { NonInteraction: 1 }
+    );
+});
