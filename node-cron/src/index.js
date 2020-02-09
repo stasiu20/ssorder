659bcb58f428b7e5e2e173d303ef7d7900a35048
Marcin Morawski <marcin@morawskim.pl>
2020-02-09 09:09:21 +0100

    #99 - obraz Docker do odpalania cyklicznych zadan
    Tymczasowe rozwiazanie. Docelowo utworzymy internal RPC API.
    Dziei temu kontener nie bedzie musial miec PHP i node.

--- node-cron/src/index.js
+++ node-cron/src/index.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const shell = require('shelljs');
 
 cron.schedule('20 8 * * *', () => {
-    shell.exec('php /app/yii task/send-mail-with-rating-link', function taskSendMailWithRatingLink(code, stdout, stderr) {
+    shell.exec('php /var/www/html/yii task/send-mail-with-rating-link', function taskSendMailWithRatingLink(code, stdout, stderr) {
         if (code !== 0) {
             console.error('Task task/send-mail-with-rating-link failed');
             console.error('Program output:', stdout);
