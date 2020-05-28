4023666c9d0f63e40d19d8faf0829a3836df3f7e
Marcin Morawski <marcin@morawskim.pl>
2020-05-28 15:10:51 +0200

    #127 - CSS modules dla restaurant card

--- frontend/webpack/js/features/restaurant/components/restaurantCard.tsx
+++ frontend/webpack/js/features/restaurant/components/restaurantCard.tsx
@@ -3,9 +3,10 @@ import { Restaurant } from '../types';
 import { useAppCtx } from '../../core/context/app';
 import { addMessages } from '../../core/translations/pl';
 import definedMessages from '../translation/pl-card';
+import styles from './restaurantCard.scss?module';
 
 interface Props {
-   restaurant: Restaurant;
+    restaurant: Restaurant;
 }
 
 const RestaurantCard: React.FC<Props> = props => {
@@ -14,49 +15,89 @@ const RestaurantCard: React.FC<Props> = props => {
 
     return (
         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-            <div className="app-card mb-5">
-                <div className="app-card__side app-card__side--front">
-                    <div className="app-card__header">
-                        <img className="app-card__img"
+            <div className={`${styles['app-card']} mb-5`}>
+                <div
+                    className={`${styles['app-card__side']} ${styles['app-card__side--front']}`}
+                >
+                    <div className={`${styles['app-card__header']}`}>
+                        <img
+                            className={`${styles['app-card__img']}`}
                             src={restaurant.imageUrl}
-                            alt={restaurant.name} />
-                        <h4 className="app-card__title"><span>{restaurant.name}</span></h4>
+                            alt={restaurant.name}
+                        />
+                        <h4 className={`${styles['app-card__title']}`}>
+                            <span>{restaurant.name}</span>
+                        </h4>
                     </div>
-                    <div className="app-card__details">
+                    <div className={`${styles['app-card__details']}`}>
                         <ul>
                             <li>
-                                <div className="d-inline-flex align-items-center">
-                                    <span className="material-icons d-inline-block mr-2">restaurant</span>
+                                <div
+                                    className={`d-inline-flex align-items-center`}
+                                >
+                                    <span
+                                        className={`material-icons d-inline-block mr-2`}
+                                    >
+                                        restaurant
+                                    </span>
                                     <span>{restaurant.category}</span>
                                 </div>
                             </li>
                             <li>
-                                <div className="d-inline-flex align-items-center">
-                                    <span className="material-icons d-inline-block mr-2">phone</span>
+                                <div
+                                    className={`d-inline-flex align-items-center`}
+                                >
+                                    <span
+                                        className={`material-icons d-inline-block mr-2`}
+                                    >
+                                        phone
+                                    </span>
                                     <span>{restaurant.telNumber}</span>
                                 </div>
                             </li>
                             <li>
-                                <div className="d-inline-flex align-items-center">
-                                    <span className="material-icons d-inline-block mr-2">local_shipping</span>
+                                <div
+                                    className={`d-inline-flex align-items-center`}
+                                >
+                                    <span
+                                        className={`material-icons d-inline-block mr-2`}
+                                    >
+                                        local_shipping
+                                    </span>
                                     <span>{restaurant.deliveryPrice}</span>
                                 </div>
                             </li>
                             <li>
-                                <div className="d-inline-flex align-items-center">
-                                    <span className="material-icons d-inline-block mr-2">shopping_basket</span>
+                                <div
+                                    className={`d-inline-flex align-items-center`}
+                                >
+                                    <span
+                                        className={`material-icons d-inline-block mr-2`}
+                                    >
+                                        shopping_basket
+                                    </span>
                                     <span>{restaurant.packPrice}</span>
                                 </div>
                             </li>
                         </ul>
-                        <button className="app-card__show-more btn btn-block btn-secondary">{appCtx.translate('Details')}</button>
+                        <button
+                            className={`${styles['app-card__show-more']} btn btn-block btn-secondary`}
+                        >
+                            {appCtx.translate('Details')}
+                        </button>
                     </div>
                 </div>
-                <div className="app-card__side app-card__side--back text-center">
-                    <div className="app-card__side-container">
+                <div
+                    className={`${styles['app-card__side']} ${styles['app-card__side--back']} text-center`}
+                >
+                    <div className={`${styles['app-card__side-container']}`}>
                         <h4 className="mb-2">{restaurant.name}</h4>
-                        <a className="btn btn-light btn-lg"
-                            href={`/restaurants/details?id=${restaurant.id}`}>{appCtx.translate('Order')}</a>
+                        <a
+                            className={`btn btn-light btn-lg`}
+                            href={`/restaurants/details?id=${restaurant.id}`}
+                        >
+                            {appCtx.translate('Order')}
+                        </a>
                     </div>
                 </div>
             </div>
