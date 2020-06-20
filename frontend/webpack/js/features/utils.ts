64705d6716dbea08d15af98e2a04e5ff4c07525e
Marcin Morawski <marcin@morawskim.pl>
2020-06-20 11:33:46 +0200

    #131 - Install symfony-ts-router npm package

--- frontend/webpack/js/features/utils.ts
+++ frontend/webpack/js/features/utils.ts
@@ -1,18 +1,21 @@
+import { Router } from 'symfony-ts-router';
+import routes from './fos_js_routes.json';
+
+type ROUTE_NAME = keyof typeof routes.routes;
+
+const router = new Router();
+router.setRoutingData(routes);
+
 export function isPwaApp(): boolean {
     return location.pathname === '/pwa';
 }
 
 export function generateUrl(
-    router: string,
+    routerName: ROUTE_NAME,
     params: Record<string, number | string>,
 ): string {
-    // todo mmo whitelist
-    switch (router) {
-        case 'restaurant':
-            return isPwaApp()
-                ? `/menu/${params.id}`
-                : `/restaurants/details?id=${params.id}`;
-        default:
-            throw new Error('Unknown router name');
+    if (isPwaApp()) {
+        routerName = `pwa-${routerName.toString()}` as ROUTE_NAME;
     }
+    return router.generate(routerName as string, params);
 }
