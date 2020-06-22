ef37148fd8e7511676c13d47983f201e46a9df99
Marcin Morawski <marcin@morawskim.pl>
2020-06-22 18:47:29 +0200

    #39 - Integration (postman) test
    We currently can't run this test during CI, because this test create new data and we need dedicated environment.

--- integration-tests/entrypoint.sh
+++ integration-tests/entrypoint.sh
@@ -3,6 +3,6 @@
 set -o nounset
 set -e
 
-for dir in /tests/tests/*/; do
+for dir in /tests/tests/[^.]*/; do
   newman run --globals /tests/globals.json --environment /tests/environments/docker.postman_environment.json --iteration-data ${dir}/data.csv  --reporters cli ${dir}/ssorder.postman_collection.json
 done
