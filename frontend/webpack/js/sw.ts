d7860d62be48daf49a39c3c08cef815b7bbe29d7
Marcin Morawski <marcin@morawskim.pl>
2020-10-19 16:53:58 +0200

    #138 Fix build PWA in production mode
    `Can't find self.__WB_MANIFEST in your SW source.` during build PWA.

--- frontend/webpack/js/sw.ts
+++ frontend/webpack/js/sw.ts
@@ -5,25 +5,24 @@ import { PrecacheEntry } from 'workbox-precaching/src/_types';
 interface Window extends ServiceWorkerGlobalScope {
     __WB_MANIFEST: Array<PrecacheEntry | string>;
 }
+declare const self: Window;
 
-function initialize(self: Window): void {
-    const filesToCache = [];
-    setCacheNameDetails({
-        prefix: 'my-app',
-        suffix: 'v1',
-        precache: 'precache',
-        runtime: 'run-time',
-        googleAnalytics: 'ga',
-    });
+// <editor-fold desc="precache">
+const filesToCache = [];
+setCacheNameDetails({
+    prefix: 'my-app',
+    suffix: 'v1',
+    precache: 'precache',
+    runtime: 'run-time',
+    googleAnalytics: 'ga',
+});
 
-    // Precaching
-    // Make sure that all the assets passed in the array below are fetched and cached
-    // The empty array below is replaced at build time with the full list of assets to cache
-    // This is done by workbox-build-inject.js for the production build
-    let assetsToCache = self.__WB_MANIFEST;
-    // To customize the assets afterwards:
-    assetsToCache = [...assetsToCache, ...filesToCache];
-    precacheAndRoute(assetsToCache);
-}
-
-initialize(self as any);
+// Precaching
+// Make sure that all the assets passed in the array below are fetched and cached
+// The empty array below is replaced at build time with the full list of assets to cache
+// This is done by workbox-build-inject.js for the production build
+let assetsToCache = self.__WB_MANIFEST;
+// To customize the assets afterwards:
+assetsToCache = [...assetsToCache, ...filesToCache];
+precacheAndRoute(assetsToCache);
+// </editor-fold>
