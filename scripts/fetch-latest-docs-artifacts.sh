dedab2c3cb6951eefcbf1aa444f07c10a413279d
Marcin Morawski <marcin@morawskim.pl>
2021-01-10 12:14:32 +0100

    #141 Fix pipeline (fetch artifacts)
    From GitLab docs (https://docs.gitlab.com/ee/ci/pipelines/job_artifacts.html#downloading-the-latest-artifacts)
    > The latest artifacts are created by jobs in the most recent successful pipeline for the specific ref.
    
    Similar issue:
    [Latest artifact download link often does not work for manually triggered jobs](https://gitlab.com/gitlab-org/gitlab/-/issues/19946)
    
    Only artifacts from the last pipeline are available.
    When the child pipeline (api_pipeline) is not called, We can't download the last artifact from one of these jobs from the child pipeline.
    Also, GitLab API doesn't include in response (for pipeline) jobs from child pipelines.
    There was an issue - [Add API endpoint for pipeline bridge jobs](https://gitlab.com/gitlab-org/gitlab/-/issues/207996)
    
    [Child pipelines and their jobs are not found by API endpoints](https://gitlab.com/gitlab-org/gitlab/-/issues/207226)
    
    [Get a list of bridge jobs for a pipeline.](https://docs.gitlab.com/ee/api/jobs.html#list-pipeline-bridges)

--- scripts/fetch-latest-docs-artifacts.sh
+++ scripts/fetch-latest-docs-artifacts.sh
@@ -3,9 +3,11 @@
 #
 #          FILE: fetch-latest-docs-artifacts.sh
 #
-#         USAGE: ./fetch-latest-docs-artifacts.sh
+#         USAGE: ./fetch-latest-docs-artifacts.sh [PROJECT_ID] [PIPELINE_ID]
 #
 #   DESCRIPTION:
+#                Arguments PROJECT_ID and PIPELINE are not mandatory
+#                if env variables CI_PROJECT_ID and CI_PIPELINE_ID exists
 #
 #       OPTIONS: ---
 #  REQUIREMENTS: ---
@@ -19,15 +21,43 @@
 
 set -euo pipefail
 set -o nounset                              # Treat unset variables as an error
+shopt -s inherit_errexit
 IFS=$'\n\t'
+
+PROJECT_ID=${CI_PROJECT_ID:-$1}
+LC_PIPELINE_ID=${CI_PIPELINE_ID:-$2}
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ..
 
-declare -a arr=("swagger" "openapi" "redoc" "sitespeed" "codeception" "newman" "assets" "phpunit")
-for job in "${arr[@]}"
+function fetchArtefacts() {
+  local PROJECT_ID=$1
+  local LC_PIPELINE_ID=$2
+
+  curl --silent --show-error --fail "https://gitlab.com/api/v4/projects/${PROJECT_ID}/pipelines/${LC_PIPELINE_ID}/jobs" \
+    | jq '.[] | select(.status == "success" and .artifacts_file != null) | .id' \
+    | tee \
+    | xargs -I{} -n1 curl --silent --show-error --fail -L --output "{}.zip" "https://gitlab.com/api/v4/projects/${PROJECT_ID}/jobs/{}/artifacts"
+}
+
+function fetchChildPipelines() {
+  local PROJECT_ID=$1
+  local LC_PIPELINE_ID=$2
+
+  curl --silent --show-error --fail "https://gitlab.com/api/v4/projects/${PROJECT_ID}/pipelines/${LC_PIPELINE_ID}/bridges?scope=success" \
+    |  jq ".[] | .downstream_pipeline.id"
+}
+
+echo "Get artifacts for pipeline ${LC_PIPELINE_ID}"
+fetchArtefacts $PROJECT_ID $LC_PIPELINE_ID
+
+echo "Get artifacts for child pipelines"
+CHILD_PIPELINES=$(fetchChildPipelines $PROJECT_ID $LC_PIPELINE_ID)
+
+for pipelineID in "${CHILD_PIPELINES}"
 do
-   echo "Fetch artifacts for: $job"
-   curl --silent --show-error --fail -L --output "${job}.zip" "https://gitlab.com/morawskim/ssorder/-/jobs/artifacts/develop/download?job=$job"
-   unzip -d docs/artifacts ${job}.zip
+  echo "Fetch artifacts for pipeline: $pipelineID"
+  fetchArtefacts $PROJECT_ID $pipelineID
 done
+
+find -maxdepth 1 -name '*.zip' -print0 | xargs -0 -n 1 unzip -d docs/artifacts
